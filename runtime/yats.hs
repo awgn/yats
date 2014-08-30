272ded1b595699bf655f1d7865e33519c8b062e6
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-08-30 18:11:50 +0200

    Minor change

--- runtime/yats.hs
+++ runtime/yats.hs
@@ -201,7 +201,7 @@ beginWith ys xs = ys `isPrefixOf` dropWhile isSpace xs
 
 
 getOption :: [String] -> [String]
-getOption = filter (not . isBinary) . filter (not . isCppSource)
+getOption = filter (not . isBinary) . filter (not . isCppSource) . filter ("-" `isPrefixOf`)
 
 
 isCppSource :: String -> Bool
